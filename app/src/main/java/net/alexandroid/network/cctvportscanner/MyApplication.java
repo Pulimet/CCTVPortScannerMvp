c5df30f1f315b39fbbee4b6df12ccef19ab011d6
Alexey Korolev <korolev2009@gmail.com>
2018-01-07 07:18:46 +0200

    Update lib versions + AdMob app id added

--- app/src/main/java/net/alexandroid/network/cctvportscanner/MyApplication.java
+++ app/src/main/java/net/alexandroid/network/cctvportscanner/MyApplication.java
@@ -3,9 +3,12 @@ package net.alexandroid.network.cctvportscanner;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
+import com.google.android.gms.ads.MobileAds;
+
 import net.alexandroid.shpref.ShPref;
 import net.alexandroid.utils.mylog.MyLog;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MyApplication extends Application {
 
@@ -17,5 +20,7 @@ public class MyApplication extends Application {
         ShPref.init(this, ShPref.APPLY);
         MyLog.init(this);
         MyLog.setTag("ZAQ");
+
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
     }
 }
